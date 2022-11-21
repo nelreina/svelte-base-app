b711cda300896671fa391a757c36b1db26fba983
Nelson Reina (@ibis-nreina) <nelson.reina@ibis-management.com>
2022-11-21 06:10:36 -0400

    handle avatar

--- src/routes/(app)/profile/+page.svelte
+++ src/routes/(app)/profile/+page.svelte
@@ -1,17 +1,25 @@
 <script>
 	export let data;
-	let date = new Date(data?.user?.loggedInAt).toLocaleDateString();
-	let time = new Date(data?.user?.loggedInAt).toLocaleTimeString();
+	const { user } = data;
+	const POCKETBASE = import.meta.env['VITE_POCKETBASE'] || 'http://127.0.0.1:8090';
+
+	let date = new Date(user?.updated).toLocaleDateString();
+	let time = new Date(user?.updated).toLocaleTimeString();
+	let avatar = `${POCKETBASE}/api/files/users/${user?.id}/${user?.avatar}?thumb=100x100`;
 </script>
 
 <div class="he ro m in-h-screen">
 	<div class="card card-side bg-base-100 shadow-xl">
-		<figure><img src="https://placeimg.com/200/280/arch" alt="Movie" /></figure>
+		{#if user?.avatar}
+			<figure>
+				<img src={avatar} alt="Avatar" />
+			</figure>
+		{/if}
 		<div class="card-body">
 			<h2 class="card-title">Profile</h2>
 			<ul class="menu bg-base-100 w-56">
-				<li>{data?.user?.username}</li>
-				<li>{data?.user?.email}</li>
+				<li>{user?.username}</li>
+				<li>{user?.email}</li>
 				<div class="divider" />
 				<li class="text-xs">Login At</li>
 				<li class="text-xs">{date}: {time}</li>
