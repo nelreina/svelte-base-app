333da43b53364143a6b1cbbd4ed4c0b9f02e05d0
Nelson Reina (@ibis-nreina) <nelson.reina@ibis-management.com>
2022-11-22 03:05:35 -0400

    main: handle strapi login

--- src/server/config/rest-client.js
+++ src/server/config/rest-client.js
@@ -1,7 +1,15 @@
 import 'dotenv/config';
 import RestClient from '@nelreina/rest-client';
+import { API, STRAPI_USERNAME, STRAPI_PASSWORD } from '$env/static/private';
 import logger from './logger';
 
-const API = process.env['API'];
 logger.info(`API: ${API}`);
-export const restClient = new RestClient(API);
+export const restClient = new RestClient(API, { isStrapi: true });
+(async () => {
+	try {
+		logger.info(`Logging in to Strapi... ${STRAPI_USERNAME}`);
+		restClient.strapiLogin(STRAPI_USERNAME, STRAPI_PASSWORD);
+	} catch (error) {
+		logger.error(error.message);
+	}
+})();
