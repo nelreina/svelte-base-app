3e55cae644cde2498b9fac8f5d6ebecdc6dfbd58
Nelson Reina (@ibis-nreina) <nelson.reina@ibis-management.com>
2022-11-22 02:01:56 -0400

    main: added Colapse component

---
+++ src/lib/components/Colapse.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let title = "Title";
+  export let textSize = "text-xs";
+</script>
+
+<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+<div
+  tabindex="0"
+  class="collapse collapse-arrow border border-base-300 bg-base-100"
+>
+  <div class="collapse-title {textSize} text-ellipsis whitespace-nowrap">
+    {title}
+  </div>
+  <div class="collapse-content text-xs">
+    <slot><!-- optional fallback --></slot>
+  </div>
+</div>
